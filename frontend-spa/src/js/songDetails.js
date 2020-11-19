d9734a39b2a9f9bf1d238c4dd0f254cff905558a
Lauren Monowar-Jones <lvjones214@aol.com>
2020-11-19 13:12:19 -0500

    first commit

---
+++ frontend-spa/src/js/songDetails.js
@@ -0,0 +1,7 @@
+const songDetails = function(album) {
+    const songDetails = document.querySelector(".details");
+    songDetails.innerHTML = "";
+    const songDetailsHeader = document.createElement("h2");
+    songDetailsHeader.innerText = `About: ${song.songName}`;
+
+}
